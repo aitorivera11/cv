d20f46c57615e4b407dd3de84eafbbbcb4be4d4d
aitorivera11 <103530568+aitorivera11@users.noreply.github.com>
2025-07-10 13:49:24 +0000

    instalacio tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // astro.config.mjs (VERSIÓ SIMPLIFICADA)
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // Ja no necessitem la integració de Sanity aquí.
 export default defineConfig({
-  // Buidem la configuració, Astro ja sap què fer.
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
