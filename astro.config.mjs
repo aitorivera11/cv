57c66f99e81c08fc471299c5c4701ddeed917b5d
aitorivera11 <103530568+aitorivera11@users.noreply.github.com>
2025-07-10 15:19:04 +0000

    Migració a Tailwind 4 amb Vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  // No hi ha cap integració de Tailwind aquí dins.
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
