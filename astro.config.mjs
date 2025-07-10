6f5f18f8b57049dbc6e03d884cf5a058b6a98126
aitorivera11 <103530568+aitorivera11@users.noreply.github.com>
2025-07-10 14:19:36 +0000

    sseguim amb tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-        // Desactiva l'aplicació d'estils base d'Astro per evitar conflictes amb els de Tailwind.
-        applyBaseStyles: false,
-    }),
-  ]
+  // No hi ha cap integració de Tailwind aquí dins.
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,10 @@
-// tailwind.config.mjs
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
   theme: {
-    extend: {
-        // Aquí podríem afegir colors o fonts personalitzades en el futur
-        fontFamily: {
-            sans: ['Poppins', 'sans-serif'],
-        },
-    },
+    extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'), // Afegeix aquest plugin per millorar l'estil del PortableText
-  ],
+  plugins: [],
 }
