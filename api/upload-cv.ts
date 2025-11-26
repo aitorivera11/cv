bbfe5ef4944055f115bdc5f8cd84dc97ab12c2d8
aitorivera11 <103530568+aitorivera11@users.noreply.github.com>
2025-11-26 15:33:42 +0100

    Update upload-cv.ts

--- api/upload-cv.ts
+++ api/upload-cv.ts
@@ -8,8 +8,8 @@ export default async function handler(req, res) {
   try {
     const { filename, content } = req.body;
 
-    if (!process.env.BLOB_READ_WRITE_TOKEN) {
-      return res.status(500).json({ error: "No blob token provided" });
+    if (!filename || !content) {
+      return res.status(400).json({ error: "filename or content missing" });
     }
 
     const buffer = Buffer.from(content, "base64");
@@ -18,6 +18,8 @@ export default async function handler(req, res) {
       access: "public",
       token: process.env.BLOB_READ_WRITE_TOKEN,
       contentType: "application/pdf",
+      addRandomSuffix: false,        
+      cacheControlMaxAge: 0        
     });
 
     return res.status(200).json({ url });
